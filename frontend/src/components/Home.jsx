629d2e7ea213597878dfcc6388514c20ba312540
Paro0987 <mistryparomita97@mail.com>
2025-01-14 10:23:33 +0530

    Featutes updated

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,7 +2,7 @@ import {useState} from 'react'
 import Create from './Create'
 import { useEffect } from 'react'
 import axios from 'axios'
-import {BsCircleFill, BsFillTrashFill} from 'react-icons/bs'
+import {BsCircleFill, BsFillCheckCircleFill, BsFillTrashFill} from 'react-icons/bs'
 
 const Home = () => {
   const [todos, setTodos] = useState([])
@@ -11,6 +11,19 @@ const Home = () => {
          .then(result=> setTodos(result.data))
          .catch(err=> console.log(err))
   },[])
+
+  const handleEdit = (id) =>{
+    axios.put('http://localhost:3000/update/'+id)
+    .then(result=> {location.reload()})
+    .catch(err=> console.log(err))
+  }
+
+  const handleDelete = (id) =>{
+    axios.delete('http://localhost:3000/delete/'+id)
+    .then(result=>{location.reload()})
+    .catch(err=> console.log(err))
+  }
+
   return (
     <div className='home'>
         <h2>Todo List</h2>
@@ -20,12 +33,13 @@ const Home = () => {
           <div><h2>No Records</h2></div>:
           todos.map(todo=>(
             <div className='task'>
-                <div className='checkbox'>
-                <BsCircleFill className='icon'/>
-                    <p>{todo.task}</p>
+                <div className='checkbox' onClick={()=>handleEdit(todo._id)}>
+                {todo.done?<BsFillCheckCircleFill className='icon'/>: <BsCircleFill className='icon'/>}
+                
+                    <p className={todo.done?"line_through":""}>{todo.task}</p>
                  </div>
                  <div>
-                      <span><BsFillTrashFill className='icon'/></span>
+                      <span><BsFillTrashFill className='icon' onClick={()=>handleDelete(todo._id)}/></span>
                  </div>
             </div>
           ))
--- server/index.js
+++ server/index.js
@@ -23,6 +23,21 @@ app.post('/add', (req, res)=>{
     .then(result=> res.json(result))
     .catch(err=> res.json(err))
 })
+
+app.put('/update/:id', (req, res)=>{
+    const id = req.params.id;
+    TodoModel.findByIdAndUpdate({_id:id}, {done:true})
+    .then(result=> res.json(result))
+    .catch(err=> res.json(err))
+})
+
+app.delete('/delete/:id', (req, res)=>{
+    const id = req.params.id;
+    TodoModel.findByIdAndDelete({_id:id})
+    .then(result=> res.json(result))
+    .catch(err=> res.json(err))
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
 })
